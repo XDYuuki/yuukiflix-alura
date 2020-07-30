598d91e1b8d13bcc72ceba35eec13499d40def16
Gabriel Rocha <gabriel.lbr@outlook.com>
2020-07-29 23:59:04 -0300

    Just start using useState

--- frontend/yuukiflix/src/pages/cadastro/Categoria/index.js
+++ frontend/yuukiflix/src/pages/cadastro/Categoria/index.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React/*, { useState }*/ from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 
 function CadastroCategoria() {
+
+  //const [value, setValue] = useState('');
+
   return (
     <PageDefault>
       <h1>Cadastro de Categoria</h1>
 
       <form>
 
-        <label>
-          Nome da Categoria:
+        <label>          
           <input
             type="text"
+            //value={value}
           />
         </label>
 
